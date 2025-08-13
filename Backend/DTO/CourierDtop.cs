9a1d4eb069bee63074346ab08228c885b9e227b9
Vaibhav singh <117506479+Vs84303@users.noreply.github.com>
2025-08-13 08:00:37 +0530

    Create CourierDtop.cs

---
+++ Backend/DTO/CourierDtop.cs
@@ -0,0 +1,9 @@
+namespace RapidReachNET.DTO
+{
+    public class CourierDtop
+    {
+        public long Id { get; set; }
+        public string? TrackingNumber { get; set; }
+        public string? RecipientName { get; set; }
+    }
+}
